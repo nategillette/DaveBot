3fe1b97c66766e9927f5dd0500ca859f91b56441
Nate Gillette <gillette.nate@gmail.com>
2024-12-14 22:52:49 -0500

    Adding original file

---
+++ bot.py
@@ -0,0 +1,54 @@
+# bot.py
+import os
+import random
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	for guild in client.guilds:
+		if guild.name == GUILD:
+			break
+	print(
+		f'{client.user} is connected to the following guild:\n'
+		f'{guild.name}(id: {guild.id})'
+	)
+
+	members = '\n -'.join([member.name for member in guild.members])
+	print(f'Guild Members:\n - {members}')
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+
+	for_Dave = [
+	'Kill Yourself Dave',
+	'Fuck Off Dave',
+	'Eat A Bag Of Dicks Dave',
+	'Dave You Corporate Fucking Cock Sleeve'
+	]
+
+	for_Chris = [
+	'Have Another Drink Lahey'
+	]
+
+	if 'dave' in message.content.lower() or 'deaf fuck' in message.content.lower():
+		response = random.choice(for_Dave)
+		await message.channel.send(response)
+	
+
+	if 'chris' in message.content.lower():
+		response = random.choice(for_Chris)
+		await message.channel.send(response)
+
+
+
+
+client.run(TOKEN)
